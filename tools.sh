cbeceee94db1487acb80d82d123585d307961667
Randi-ID <66753543+Rendi-ID@users.noreply.github.com>
2020-06-16 21:05:44 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,472 @@
+#!/bin/bash
+
+clear
+read -p "siapa  nama ente :" nama
+echo "selamat datang di tools ane tuan" $nama
+sleep 2
+echo "sebelum menggunakan tools ini"
+sleep 2
+echo "alangkan baiknya kita berantem dulu"
+sleep 2
+echo "kenapa?takut..lemah!!!"
+sleep 2
+clear
+figlet WALCOME | lolcat
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬" | lolcat
+echo "[•] Author : Randiansyah" | lolcat
+echo "[•] Youtube: Rendy Noober" | lolcat
+echo "[•] Github : github.com/Rendi-ID" | lolcat
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬" | lolcat
+echo "(1) MUCULKAN SEMUA TOMBOL" | lolcat
+echo "===============================================" | lolcat
+echo "(2) HACK AKUN IG" | lolcat
+echo "===============================================" | lolcat
+echo "(3) DDOS LUCITA" |lolcat
+echo "===============================================" | lolcat
+echo "(4) DDOS TROJAN(update)" | lolcat
+echo "===============================================" | lolcat
+echo "(5) LITEDDOS" | lolcat
+echo "===============================================" | lolcat
+echo "(6) VIRUS PERUSAK HP" | lolcat
+echo "===============================================" | lolcat
+echo "(7) VIRTEX FOR WHATSAPP " | lolcat
+echo "===============================================" | lolcat
+echo "(8) HACK  FB TEMAN" | lolcat
+echo "===============================================" | lolcat
+echo "(9) METASPLOIT " | lolcat
+echo "===============================================" | lolcat
+echo "(10) CEK IP SAYA" | lolcat
+echo "===============================================" | lolcat
+echo "(11) SADAP WHATSAPP" | lolcat
+echo "===============================================" | lolcat
+echo "(12) UBUNTU" | lolcat
+echo "===============================================" | lolcat
+echo "(13) DEBIAN" | lolcat
+echo "===============================================" | lolcat
+echo "(14) KALI" | lolcat
+echo "===============================================" | lolcat
+echo "(15) NET HUNTER" | lolcat
+echo "===============================================" | lolcat
+echo "(16) HACK FB PREMIUM" | lolcat
+echo "===============================================" | lolcat
+echo "(17) LACAK IP" | lolcat
+echo "===============================================" | lolcat
+echo "(18) SPAM SMS" | lolcat
+echo "===============================================" | lolcat
+echo "(19) BotChat" | lolcat
+echo "===============================================" | lolcat
+echo "(20)  AUTO BOOT FACEBOOK" | lolcat
+echo "===============================================" | lolcat
+echo "(21) OSIF" | lolcat
+echo "===============================================" | lolcat
+echo "(22) CRACK AKUN FB" | lolcat
+echo "===============================================" | lolcat
+echo "(23) BAJINGANv6" | lolcat
+echo "===============================================" | lolcat
+echo "(24) HACK FB TARGET" | lolcat
+echo "===============================================" | lolcat
+echo "(25) BOBOL WIFI" | lolcat
+echo "===============================================" | lolcat
+echo "(26) Archlinu" | lolcat
+echo "===============================================" | lolcat
+echo "(27) SPAM SMS MAPCLUB UNLI" | lolcat
+echo "===============================================" | lolcat
+echo "(28) KELUAR" | lolcat
+echo "===============================================" | lolcat
+read -p "Masukan pilihan anda disini :" pilih;
+if [ $pilih = "1" ]
+then
+    echo "installing terkey..." | lolcat
+    sleep 2
+    cd $HOME
+    apt update && apt upgrade
+    apt install git
+    pip install mechanize
+    pip install requests
+    pkg install python
+    git clone https://github.com/karjok/terkey
+    echo "penginstallan selesai..." | lolcat
+    sleep 2
+    cd terkey
+    python terkey.py
+elif [ $pilih = "2" ]
+then
+    echo "installing bot-igeh1..." | lolcat
+    sleep 2
+    cd $HOME
+    apt update && apt upgrade
+    pkg install git &&
+    pip2 install mechanize
+    pip2 install requests
+    pkg install nodejs
+    git clone https://github.com/ikiganteng/bot-igeh1
+    echo "penginstallan selesai..." | lolcat
+    sleep 2
+    cd bot-igeh1
+    unzip node_modules.zip
+    node index
+elif [ $pilih = "3" ]
+then
+    echo "installing lucita_ddos" | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    git clone https://github.com/zlucifer/lucita_ddos
+    pip install mechanize
+    pip install requests
+    pkg install python
+    echo "penginstallan selesai..." | lolcat
+    sleep 2
+    cd lucita_ddos
+    chmod +x pukul.py
+    python pukul.py
+elif [ $pilih = "4" ]
+then
+    echo "installing DOS-TOOKITS..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pip install mechanize
+    pip install request
+    pkg install bashs
+    git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+    echo "penginstallan selesai..." | lolcat
+    sleep 2
+    cd DOS-TOOKITS
+    chmod +x DOS-TOOKITS.sh
+    bash DOS-TOOKITS.sh
+elif [ $pilih = "5" ]
+then
+    echo "installing LITEDDOS..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrad
+    pkg install git
+    pip2 install mechanize
+    pip2 install requests
+    pkg install python2
+    git clone https://github.com/4L13199/LITEDDOS
+    echo "penginstallan selesai..." | lolcat
+    sleep 2
+    cd LITEDDOS
+    chmod +x liteDDOS.py
+    python2 liteDDOS.py
+elif [ $pilih = "6" ]
+then
+    echo "installing perusak..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pip2 install mechanize
+    pip2 install requests
+    pkg install python2
+    git clone https://github.com/justahackers/perusak
+    echo "penginstallan selesai..." | lolcat
+    sleep 2
+    cd perusak
+    python2 perusak.py
+elif [ $pilih = "7" ]
+then
+    echo "install VIRTEX..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pip2 install mechanize
+    pip2 install requests
+    pkg install bash
+    git clone https://github.com/muhammadfathul/VIRTEX
+    echo "penginstallan selesai..." | lopcat
+    cd VIRTEX
+    chmod +x virtex.sh
+    bash virtex.sh
+elif [ $pilih = "8" ]
+then
+    echo "installing hack-facebook-teman..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pip2 install mechanize
+    pip2 install requests
+    pkg install python2
+    git clone https://github.com/blackcodercrush/hack-facebook-teman
+    echo "penginstallan selesai..." | lolcat
+    cd hack-facebook-teman
+    bash requests.sh
+    python2 hack-fb.py
+elif [ $pilih = "9" ]
+then
+    echo "proses menginstallan butuh proses yang lama" | lolcat
+    sleep 6
+    echo "dan memakan data kurang lebih 300 mb" | lolcat
+    sleep 10
+    echo "proses installasi tergantung jaringan internet" | lolcat
+    sleep 7
+    echo "installing metasploit..." | lolcat
+    sleep 5
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install wget
+    pkg install bash
+    wget https://5hady.github.io/metasploit.sh
+    bash metasploit.sh
+    echo "penginstallan selesai..." | lolcat
+    cd metasploit-framework
+    ./msfconsole
+elif [ $pilih = "10" ]
+then
+    echo "installing My-Ip..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pip install mechanize
+    pip install requests
+    pkg install python
+    git clone https://github.com/Bl4ckDr460n/My-Ip
+    echo "penginstallan selesai..." | lolcat
+    cd My-Ip
+    python My-ip.py
+elif [ $pilih = "11" ]
+then
+    echo "installing HxWhatsapp..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pip install mechanize
+    pip install requests
+    pkg install python2
+    git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+    echo "penginstallan selesai..." | lolcat
+    cd HxWhastapp
+    python2 HxWhatsApp.py
+elif [ $pilih = "12" ]
+then
+   echo "installing ubuntu..." | lolcat
+   sleep 2
+   cd $HOME
+   pkg update && pkg upgrade
+   pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Debian/debian.sh && bash debian.sh
+   echo "penginstallan selesai..." | lolcat
+   ./start-ubuntu.sh
+elif [ $pilih = "13" ]
+then
+   echo "installing debian..." | lolcat
+   sleep 2
+   cd $HOME
+   pkg update && pkg  upgrade
+   pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Debian/debian.sh && bash debian.sh
+   echo "penginstallan selesai..." | lolcat
+   ./start-debian.sh
+elif [ $pilih = "14" ]
+then
+    echo "installing kali..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kal1
+    echo "penginstallan selesai..." | lolcat
+    ./start-kali.sh
+elif [ $pilih = "15" ]
+then
+    echo "installing net hunter..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install curl
+    pkg install git
+    curl -LO https://raw.githubusercontent.com/Hax4us/Nethunter-In-Termux/master/kalinethunter
+    echo "penginstallan selesai..." | lolcat
+elif [ $pilih = "16" ]
+then
+    echo "installing Hack-Fb-Premium..."
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install python2
+    pip2 install mechanize
+    pip2 install requests
+    git clone https://github.com/Bl4ckDr460n/Black-Fb-Premium
+    echo "selesai..."
+    sleep 2
+    clear
+    cd Black-Fb-Premium
+    python2 Black-Fb.py
+elif [ $pilih = "17" ]
+then
+    echo "install "
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install python2
+    pip2 install mechanize
+    pip2 install requests
+    git clone https://github.com/Bl4ckDr460n/IP-Location
+    echo "selesai..."
+    sleep 2
+    clear
+    cd IP-Location
+    python2 lacak.py
+elif [ $pilih = "18" ]
+then
+    echo "installing SPAMer..."
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg intsall git
+    pkg install python python2
+    pip2 install mechanize
+    pip2 install requests
+    git clone https://github.com/Bl4ckDr460n/SPAMer
+    echo "selesai..."
+    sleep 2
+    clear
+    cd SPAMer
+    python2 SPAMer.py
+elif [ $pilih = "19" ]
+then
+    echo "installing BotChat..."
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install python2
+    pip2 install mechanize
+    pip2 install requests
+    git clone https://github.com/Bl4ckDr460n/BotChat
+    echo "selesai..." | lolcat
+    sleep 2
+    cd BotChat
+    python2 BotChat.py
+elif [ $pilih = "20"]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install python2
+    pip2 install mechanize
+    pip2 install requests
+    git clone https://github.com/Senitopeng/BotFbBangDjon.git
+    echo "selesai..."| lolcat
+    cd BotFbBangDjon
+    python2 bangdjon.py
+elif [ $pilih = "21" ]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install python2
+    pip2 install mechanize
+    pip2 install requests
+    git clone https://github.com/ciku370/OSIF
+    echo "selesai..." | lolcat
+    cd osif
+    python2 osif.py
+elif [ $pilih = "22" ]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install python2
+    pkg install git
+    pip2 install mechanize
+    pip2 install requests
+    git clone https://github.com/Makky2693/Hack-FB
+    echo "selesai..." | lolcat
+    sleep 2
+    cd Hack-FB
+    python2 Hack-FB.py
+elif [ $pilih = "23" ]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install bash
+    pip2 install mechanize
+    git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+    echo "selesai..."
+    sleep 2
+    cd BAJINGANv6
+    bash BAJINGAN.sh
+    BAJINGAN
+    Gans
+elif [ $pilih = "24" ]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install bash
+    pip install mechanize
+    pip install requests
+    git clone https://github.com/FR13ND8/BRUTEFORCEnew
+    echo "selesai..."| lolcat
+    sleep 2
+    cd BRUTEFORCEnew
+    bash new.sh
+elif [ $pilih = "25" ]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install bash
+    pip install mechanize
+    pip install requests
+    chmod +x wifi-hacker.sh
+    bash wifi-hacker.sh
+    echo "pilih y tunggu 5-10 detik lalu pilih 2" | lolcat
+    echo "buka menu wifi di hp terus tutup" | lolcat
+    echo "buka lagi, automatis terhubung" | lolcat
+elif [ $pilih = "26" ]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install bash
+    pip install mechanize
+    pip install requests
+    git clone https://github.com/zlucifer/light_bringer
+    echo "selesai..." | lolcat
+    cd light_bringer
+    chmod +x *
+    bash light.sh
+elif [ $pilih = "27" ]
+then
+    echo "installing..." | lolcat
+    sleep 2
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install git
+    pkg install python2
+    git clone https://github.com/Tn-Cr4b/spam-mapclub.git
+    echo "selesai..." | lolcat
+    sleep
+    cd spam-mapclub
+    python2 gas.py
+elif [ $pilih = "28" ]
+then
+    echo "Terima Kasih Karna Ente Sudah Menggunakan Tools Ane" | lolcat
+    sleep 2
+    clear
+    exit
+else
+    echo "masukan input yang bener anj*ng bikin emosi aja ni org, keluar" | lolcat
+    sleep 5
+    bash tools.sh
+fi
